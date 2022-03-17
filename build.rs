c958e166ad2ee87b1051d5b5c9eb388ef152e605
Jia Ye <planet0104@gmail.com>
2022-03-17 20:30:26 +0800

    add info

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 fn main() {
     if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("favicon.ico");
-        res.compile().unwrap();
+        winres::WindowsResource::new()
+        .set_icon("favicon.ico")
+        .set("InternalName", "MATRIX.EXE")
+        .compile().unwrap();
     }
 }
